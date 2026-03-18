82a00c3873d34819e5da4cc6953bf6e96b5bf38b
Permana <sdharma721@gmail.com>
2026-03-18 22:18:44 +0700

    fix(exception): simplified OrderNotFoundException constructor

--- src/Java_Generics/ecommerce/exception/OrderNotFoundException.java
+++ src/Java_Generics/ecommerce/exception/OrderNotFoundException.java
@@ -1,7 +1,7 @@
 package Java_Generics.ecommerce.exception;
 
 public class OrderNotFoundException extends RuntimeException {
-  public OrderNotFoundException(String message) {
-    super(message);
-  }
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
 }
