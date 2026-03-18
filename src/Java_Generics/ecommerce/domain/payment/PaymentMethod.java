a16d1a8239f847f080bae4192323ba08bb0f9f4e
Permana <sdharma721@gmail.com>
2026-03-18 22:18:28 +0700

    refactor(payment): Introduce new payment methods: Bank, Qris, Tunai with descriptions

--- src/Java_Generics/ecommerce/domain/payment/PaymentMethod.java
+++ src/Java_Generics/ecommerce/domain/payment/PaymentMethod.java
@@ -1,4 +1,13 @@
 package Java_Generics.ecommerce.domain.payment;
 
 public enum PaymentMethod {
+    BANK("Bank"),
+    QRIS("Qris"),
+    TUNAI("Tunai");
+
+    private String description;
+
+    PaymentMethod(String description) {
+        this.description = description;
+    }
 }
