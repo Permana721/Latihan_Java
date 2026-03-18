a34f8f9304d6606c015163de07670ee7431a04b2
Permana <sdharma721@gmail.com>
2026-03-18 22:18:16 +0700

    feat(OrderStatus): add Packed, Sent, OTW, Delivered enum values with descriptions

--- src/Java_Generics/ecommerce/domain/order/OrderStatus.java
+++ src/Java_Generics/ecommerce/domain/order/OrderStatus.java
@@ -1,4 +1,14 @@
 package Java_Generics.ecommerce.domain.order;
 
 public enum OrderStatus {
+    PACKED("Packed"),
+    SENT("Sent"),
+    OTW("On The Way"),
+    DELIVERED("Delivered");
+
+    private String description;
+
+    OrderStatus(String description) {
+        this.description = description;
+    }
 }
