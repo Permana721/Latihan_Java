ca4ee7690f5f43a2ce91b8f6a1d16ef430af6835
Permana <sdharma721@gmail.com>
2026-03-18 22:18:25 +0700

    refactor(category): add descriptions for enum constants

--- src/Java_Generics/ecommerce/domain/product/Category.java
+++ src/Java_Generics/ecommerce/domain/product/Category.java
@@ -1,4 +1,14 @@
 package Java_Generics.ecommerce.domain.product;
 
 public enum Category {
+    ELECTRONICS("Electronic"),
+    FOOD("Food"),
+    FASHION("Fashion"),
+    GROCERIES("Groceries");
+
+    private String description;
+
+    Category(String description){
+        this.description = description;
+    }
 }
