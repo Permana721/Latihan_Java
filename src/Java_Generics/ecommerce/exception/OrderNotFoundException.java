3b0edb80f31435162d70e3c96c153b3d287ea13b
Permana <sdharma721@gmail.com>
2026-03-19 20:27:50 +0700

    refactor(exception): remove unnecessary data from OrderNotFoundException constructor

--- src/Java_Generics/ecommerce/exception/OrderNotFoundException.java
+++ src/Java_Generics/ecommerce/exception/OrderNotFoundException.java
@@ -3,5 +3,6 @@ package Java_Generics.ecommerce.exception;
 public class OrderNotFoundException extends RuntimeException {
     public OrderNotFoundException(String message) {
         super(message);
+        // no data
     }
 }
