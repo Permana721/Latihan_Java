e945e4e6b1b9273725710c21f5b983765077df06
Permana <sdharma721@gmail.com>
2026-03-24 13:52:04 +0700

    feat(product-repository): implemented CRUD operations for ProductRepository

--- src/Java_Generics/ecommerce/repository/ProductRepository.java
+++ src/Java_Generics/ecommerce/repository/ProductRepository.java
@@ -1,5 +1,34 @@
 package Java_Generics.ecommerce.repository;
 
-public class ProductRepository {
-    // no data
+import Java_Generics.ecommerce.domain.product.Product;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepository implements Repository<Product> {
+    private ArrayList<Product> allProduct = new ArrayList<>();
+    private int nextId = 1;
+
+    @Override
+    public void save(Product item) {
+        item.setId(nextId++);
+        allProduct.add(item);
+    }
+
+    @Override
+    public void delete(int id) {
+        allProduct.removeIf(p -> p.getId() == id);
+    }
+
+    @Override
+    public Product findById(int id) {
+        for (Product p : allProduct) {
+            if (p.getId() == id) return p;
+        }
+        return null;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return allProduct;
+    }
 }
