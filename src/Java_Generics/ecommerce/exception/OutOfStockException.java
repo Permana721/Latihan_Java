e3a8e1f7b8f3605cd8564f57184eeea904bbbb30
Permana <sdharma721@gmail.com>
2026-03-24 19:54:34 +0700

    feat(OutOfStockException): add id and name constructors for exception message

--- src/Java_Generics/ecommerce/exception/OutOfStockException.java
+++ src/Java_Generics/ecommerce/exception/OutOfStockException.java
@@ -1,8 +1,11 @@
 package Java_Generics.ecommerce.exception;
 
 public class OutOfStockException extends RuntimeException {
-    public OutOfStockException(String message) {
-        super(message);
-         // no data
+    public OutOfStockException(int id) {
+        super("Stok produk dengan id " + id + " sudah habis!\n");
+    }
+
+    public OutOfStockException(String name) {
+        super("Stok produk dengan nama " + name + " sudah habis!\n");
     }
 }
