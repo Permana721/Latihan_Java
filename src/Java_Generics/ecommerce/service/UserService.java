fe24f99e677d428d35dcc670bb4ebe8017f417e0
Permana <sdharma721@gmail.com>
2026-03-24 19:54:56 +0700

    feat(UserService): Implement user registration, deletion and retrieval

--- src/Java_Generics/ecommerce/service/UserService.java
+++ src/Java_Generics/ecommerce/service/UserService.java
@@ -1,4 +1,11 @@
 package Java_Generics.ecommerce.service;
 
+import Java_Generics.ecommerce.domain.user.User;
+
+import java.util.List;
+
 public interface UserService {
+    void register(String name, String email);
+    void deleteUser(int id);
+    List<User> getAllUsers();
 }
