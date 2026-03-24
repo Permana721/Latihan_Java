d15d411a5cbc2936e86d38e8000acd9d592df7b9
Permana <sdharma721@gmail.com>
2026-03-24 13:51:52 +0700

    refactor(category): add getDescription method

--- src/Java_Generics/ecommerce/domain/product/Category.java
+++ src/Java_Generics/ecommerce/domain/product/Category.java
@@ -11,4 +11,8 @@ public enum Category {
     Category(String description){
         this.description = description;
     }
+
+    public String getDescription() {
+        return description;
+    }
 }
