40680dc18861c11844f0e549e88dc920eb017c64
Permana <sdharma721@gmail.com>
2026-03-24 13:52:15 +0700

    feat(user-repository): implemented CRUD operations for UserRepository

--- src/Java_Generics/ecommerce/repository/UserRepository.java
+++ src/Java_Generics/ecommerce/repository/UserRepository.java
@@ -1,5 +1,34 @@
 package Java_Generics.ecommerce.repository;
+import Java_Generics.ecommerce.domain.user.User;
 
-public class UserRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+public class UserRepository implements Repository<User> {
+    private ArrayList<User> allUser = new ArrayList<>();
+    private int nextId = 1;
+
+    @Override
+    public void save(User item) {
+        item.setId(nextId++);
+        allUser.add(item);
+    }
+
+    @Override
+    public void delete(int id) {
+        allUser.removeIf(p -> p.getId() == id);
+    }
+
+    @Override
+    public User findById(int id) {
+        for (User p : allUser) {
+            if (p.getId() == id) return p;
+        }
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return allUser;
+    }
 }
