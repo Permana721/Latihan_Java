26d12ef79f903185089785375973dbb1aa5b7654
Permana <sdharma721@gmail.com>
2026-03-24 19:54:37 +0700

    feat(exception): Added constructors for ProductNotFoundException with id and name parameters

--- src/Java_Generics/ecommerce/exception/ProductNotFoundException.java
+++ src/Java_Generics/ecommerce/exception/ProductNotFoundException.java
@@ -1,5 +1,11 @@
 package Java_Generics.ecommerce.exception;
 
 public class ProductNotFoundException extends Exception{
+    public ProductNotFoundException(int id) {
+        super("Produk dengan id " + id + " tidak ditemukan\n");
+    }
 
+    public ProductNotFoundException(String name) {
+        super("Produk dengan nama " + name + " tidak ditemukan\n");
+    }
 }
