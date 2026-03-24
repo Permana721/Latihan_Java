5c99f6f86ea6cb34c1cb8b66fa0958d74ffa5983
Permana <sdharma721@gmail.com>
2026-03-24 19:54:27 +0700

    refactor(product): add stock attribute and related methods

--- src/Java_Generics/ecommerce/domain/product/Product.java
+++ src/Java_Generics/ecommerce/domain/product/Product.java
@@ -4,17 +4,20 @@ public class Product {
     private int id;
     private String name;
     private int price;
+    private int stock;
     private Category category;
 
-    public Product(int id, String laptop, int price) {
 
-    }
-
-    public Product(int id, String name, int price, Category category) {
+    public Product(int id, String name, int price, int stock, Category category) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.stock = stock;
         this.category = category;
+    }
+
+    public Product(int id, String laptop, int price) {
+
     }
 
     public int getId() {
@@ -41,6 +44,14 @@ public class Product {
         this.price = price;
     }
 
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
     public Category getCategory() {
         return category;
     }
@@ -52,6 +63,7 @@ public class Product {
     public void printInfo(){
         System.out.println("Nama: " + name);
         System.out.println("Harga: " + price);
+        System.out.println("Stok: " + stock);
         System.out.println("Kategori: " + category.getDescription());
         System.out.println("===========================");
     }
