03579b2a9dc692cd3e99b0a4de80cb2716a51dd1
Permana <sdharma721@gmail.com>
2026-03-24 11:19:08 +0700

    feat(product): implement Product class with properties and methods for name, price, category, and printInfo

--- src/Java_Generics/ecommerce/domain/product/Product.java
+++ src/Java_Generics/ecommerce/domain/product/Product.java
@@ -1,4 +1,48 @@
 package Java_Generics.ecommerce.domain.product;
 
-public abstract class Product {
+public class Product {
+    private String name;
+    private int price;
+    private Category category;
+
+    public Product(String laptop, int price) {
+
+    }
+
+    public Product(String name, int price, Category category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void printInfo(){
+        System.out.println("Nama: " + name);
+        System.out.println("Harga: " + price);
+        System.out.println("Kategori: " + category);
+        System.out.println("===========================");
+    }
 }
