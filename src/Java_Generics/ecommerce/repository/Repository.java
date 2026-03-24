d8ab6b2645f4d3d364bbad3e04a42d02c88cf1ae
Permana <sdharma721@gmail.com>
2026-03-24 19:54:41 +0700

    feat(Repository): Add `delete` method with exception handling, introduce `findByName` method

--- src/Java_Generics/ecommerce/repository/Repository.java
+++ src/Java_Generics/ecommerce/repository/Repository.java
@@ -1,10 +1,13 @@
 package Java_Generics.ecommerce.repository;
 
+import Java_Generics.ecommerce.domain.product.Product;
+import Java_Generics.ecommerce.exception.ProductNotFoundException;
+
 import java.util.List;
 
 public interface Repository<T> {
     void save(T item);
-    void delete(int id);
-    T findById(int id);
+    void delete(int id) throws ProductNotFoundException;
+    Product findByName(String name) throws ProductNotFoundException;
     List<T> findAll();
 }
