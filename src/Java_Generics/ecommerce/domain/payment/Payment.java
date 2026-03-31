d3b31da039b781d7c83924aaf455d09f771fc21e
Permana <sdharma721@gmail.com>
2026-03-31 10:45:05 +0700

    feat(payment): Introduce payment amount and method fields, constructors, and getters/setters

--- src/Java_Generics/ecommerce/domain/payment/Payment.java
+++ src/Java_Generics/ecommerce/domain/payment/Payment.java
@@ -1,4 +1,27 @@
 package Java_Generics.ecommerce.domain.payment;
 
 public class Payment {
+    private int jmlBayar;
+    private PaymentMethod metode;
+
+    public Payment(int jmlBayar, PaymentMethod metode) {
+        this.jmlBayar = jmlBayar;
+        this.metode = metode;
+    }
+
+    public int getJmlBayar() {
+        return jmlBayar;
+    }
+
+    public void setJmlBayar(int jmlBayar) {
+        this.jmlBayar = jmlBayar;
+    }
+
+    public PaymentMethod getMetode() {
+        return metode;
+    }
+
+    public void setMetode(PaymentMethod metode) {
+        this.metode = metode;
+    }
 }
