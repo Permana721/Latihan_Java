1fd0a5d055ab3c5f22921edd3eb02b578bc9b784
Permana <sdharma721@gmail.com>
2026-03-31 10:53:09 +0700

    feat(login): implement user login validation and check existence

--- src/Java_Generics/ecommerce/service/UserServiceImpl.java
+++ src/Java_Generics/ecommerce/service/UserServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     private UserRepository userRepo;
     private int nextId = 1;
-    private int balance = 500_000;
+    private int balance = 0;
 
     public UserServiceImpl(UserRepository userRepo) {
         this.userRepo = userRepo;
@@ -35,6 +35,26 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public User login(String name, String email) {
+
+        if (name == null || name.isEmpty() || email == null || email.isEmpty()) {
+            throw new InvalidUserException("Nama dan email tidak boleh kosong");
+        }
+        if (!email.contains("@")) {
+            throw new InvalidUserException("Format email tidak valid");
+        }
+
+        for (User user : userRepo.findAll()) {
+            if (user.getName().equalsIgnoreCase(name) && user.getEmail().equals(email)) {
+                System.out.println("✓ Login berhasil! Selamat datang, " + user.getName());
+                return user;
+            }
+        }
+
+        throw new InvalidUserException("Nama atau email salah");
+    }
+
+    @Override
     public void deleteUser(int id) {
         userRepo.delete(id);
         System.out.println("✓ User id=" + id + " dihapus");
@@ -44,4 +64,13 @@ public class UserServiceImpl implements UserService {
     public List<User> getAllUsers() {
         return userRepo.findAll();
     }
+
+    @Override
+    public void tambahSaldo(int userId, int jumlah) {
+        User user = userRepo.findUserById(userId);
+
+        user.setSaldo(user.getSaldo() + jumlah);
+        System.out.println("Saldo berhasil ditambah: Rp." + jumlah + "\nSaldo sekarang: Rp." + user.getSaldo() + "\n");
+    }
+
 }
