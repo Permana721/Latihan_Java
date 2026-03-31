9d00f1158bf6c3dc31b9662f1f6c84f612f08889
Permana <sdharma721@gmail.com>
2026-03-31 10:52:49 +0700

    refactor(product_service): introduce `ProductNotFoundException` for buying product operation

--- src/Java_Generics/ecommerce/service/ProductService.java
+++ src/Java_Generics/ecommerce/service/ProductService.java
@@ -2,10 +2,12 @@ package Java_Generics.ecommerce.service;
 
 import Java_Generics.ecommerce.domain.product.Category;
 import Java_Generics.ecommerce.domain.product.Product;
+import Java_Generics.ecommerce.exception.ProductNotFoundException;
 
 import java.util.List;
 
 public interface ProductService {
     void addProduct(String name, int price, int stock, Category category);
+    void buyProduct(int userId, String name) throws ProductNotFoundException;
     List<Product> getAllProduct();
 }
