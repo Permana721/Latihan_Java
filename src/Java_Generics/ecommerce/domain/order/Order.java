6c4e5d7f392bcf63f90d58a1a09f7266af8e2f2f
Permana <sdharma721@gmail.com>
2026-03-31 10:45:00 +0700

    feat(order): add item list, total price, and order status to Order class

--- src/Java_Generics/ecommerce/domain/order/Order.java
+++ src/Java_Generics/ecommerce/domain/order/Order.java
@@ -1,4 +1,37 @@
 package Java_Generics.ecommerce.domain.order;
 
 public class Order {
+    private String daftarItem;
+    private int totalHarga;
+    private OrderStatus status;
+
+    public Order(String daftarItem, int totalHarga, OrderStatus status) {
+        this.daftarItem = daftarItem;
+        this.totalHarga = totalHarga;
+        this.status = status;
+    }
+
+    public String getDaftarItem() {
+        return daftarItem;
+    }
+
+    public void setDaftarItem(String daftarItem) {
+        this.daftarItem = daftarItem;
+    }
+
+    public int getTotalHarga() {
+        return totalHarga;
+    }
+
+    public void setTotalHarga(int totalHarga) {
+        this.totalHarga = totalHarga;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
