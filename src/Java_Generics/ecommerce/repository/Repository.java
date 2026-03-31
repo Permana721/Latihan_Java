8694b1155627760697d53e0bbef77ff194d7fcf4
Permana <sdharma721@gmail.com>
2026-03-31 10:45:16 +0700

    fix(repository): refactor find methods for consistency (product, user)

--- src/Java_Generics/ecommerce/repository/Repository.java
+++ src/Java_Generics/ecommerce/repository/Repository.java
@@ -1,6 +1,8 @@
 package Java_Generics.ecommerce.repository;
 
 import Java_Generics.ecommerce.domain.product.Product;
+import Java_Generics.ecommerce.domain.user.User;
+import Java_Generics.ecommerce.exception.InvalidUserException;
 import Java_Generics.ecommerce.exception.ProductNotFoundException;
 
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.List;
 public interface Repository<T> {
     void save(T item);
     void delete(int id) throws ProductNotFoundException;
-    Product findByName(String name) throws ProductNotFoundException;
+    Product findProductByName(String name) throws ProductNotFoundException;
+    User findUserById(int id) throws InvalidUserException;
     List<T> findAll();
 }
